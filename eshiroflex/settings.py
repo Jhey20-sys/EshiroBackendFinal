5ef0c1c878b3e6806fb4209c48f9a40f559e6e27
6reenhorn <coding.cyrilj777@gmail.com>
2025-03-19 16:42:03 +0800

    Modified settings.py

--- eshiroflex/settings.py
+++ eshiroflex/settings.py
@@ -100,7 +100,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
